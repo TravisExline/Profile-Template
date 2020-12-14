2d0af67d2823982ab095de9a58c8096f1eb3cdee
TravisExline <t.exline22@gmail.com>
2020-12-14 09:41:45 -0600

    first

---
+++ src/containers/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    Search / Navlink
+                </section>
+                <section>
+                    Profile Header
+                </section>
+                <section>
+                    Sidebar
+                </section>
+                <section>
+                    Feed
+                </section>
+                <section>
+                    Footer?
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Profile
